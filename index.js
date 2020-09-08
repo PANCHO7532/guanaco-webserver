38780aeb87861aab1c117d3402a12ef3bcfea387
PANCHO7532 <pancho7532@protonmail.com>
2020-09-08 01:27:11 -0300

    P7RND_1293300

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const https = require('https');
 const fs = require('fs');
 const sslPrefs = require('./lib/sslPrefs');
 const httpHandler = require('./lib/httpHandler');
-const mainPort = 80;
-const mainSSLPort = 443;
-const mainServer = http.createServer(function(req, res) { httpHandler.main(req, res); });
-const mainServer2 = https.createServer({cert: sslPrefs.getSSL("cert"), key: sslPrefs.getSSL("key")}, function(req, res) { httpHandler.main(req, res, true); });
+const configFile = require('./lib/genericUtils').configFile();
 
-mainServer.listen(mainPort, function() {
-    console.log("[INFO] - Server running on port: " + mainPort);
+const mainServer = http.createServer(function(req, res) { httpHandler.main(configFile["webrootPath"], req, res); });
+const mainServer2 = https.createServer({cert: sslPrefs.getSSL("cert"), key: sslPrefs.getSSL("key")}, function(req, res) { httpHandler.main(configFile["webrootPath"], req, res, true); });
+
+mainServer.listen(configFile["httpPort"], function() {
+    console.log("[INFO] - Server running on port: " + configFile["httpPort"]);
 });
-mainServer2.listen(mainSSLPort, function() {
-    console.log("[INFO] - SSL Server running on port: " + mainSSLPort);
+mainServer2.listen(configFile["httpsPort"], function() {
+    console.log("[INFO] - SSL Server running on port: " + configFile["httpsPort"]);
 });
